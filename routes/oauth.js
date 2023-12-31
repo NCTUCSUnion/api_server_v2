d48a2cad0cee43f8384a674b23fe592b8569e2c5
Jay Lee <jayin920805@gmail.com>
2024-01-01 02:28:55 +0800

    fixme: bypass ssl expired certificate

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,11 +1,15 @@
 const axios = require('axios')
 const qs = require('querystring')
+const https = require('https')
 
 module.exports = (client_id, client_secret, redirect_uri, profile_callback) => ({
     login: (req, res) => {
         res.redirect(`https://id.nycu.edu.tw/o/authorize/?client_id=${client_id}&response_type=code&scope=profile`)
     },
     auth: (req, res, next) => {
+        const agent = new https.Agent({  
+            rejectUnauthorized: false // bypass certificate error
+          });
         const code = req.query.code
         axios.post('https://id.nycu.edu.tw/o/token/',
             qs.stringify({
@@ -15,7 +19,8 @@ module.exports = (client_id, client_secret, redirect_uri, profile_callback) => (
                 client_secret,
                 redirect_uri
             }), {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            httpsAgent: agent
         })
             .then(res => res.data)
             .then(
